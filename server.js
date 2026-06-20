0a091bcc1f2561c23deafbbde204309909bc8935
EliteShoot <66973298+EliteShoot@users.noreply.github.com>
2026-06-20 13:27:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
